1b9e87e14ed845557e4f0e55a92c198b491afda0
Tarun <gtarun1991@gmail.com>
2016-11-15 23:38:44 +1100

    shortcuts explained

---
+++ Test_Program/Test_Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Short Cuts Learnt
+            // For commenting the code use control+k and control+c
+            // For building and running the program use F5 key 
+        }
+    }
+}
